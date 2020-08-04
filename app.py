781ce7f864d37c346cf0f73a7c0c71f7a732ad11
Chuck Lee <Chuck.Lee@aver.com>
2020-08-04 18:10:49 +0800

    Demo

--- app.py
+++ app.py
@@ -14,7 +14,8 @@
 
 import os
 import sys
-import wsgiref.simple_server
+import random
+from wsgiref.simple_server import make_server
 from argparse import ArgumentParser
 
 from builtins import bytes
@@ -25,27 +26,28 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+    MessageEvent, TextMessage, StickerMessage, TextSendMessage, ImageSendMessage, StickerSendMessage
 )
 from linebot.utils import PY3
 
 # get channel_secret and channel_access_token from your environment variable
-# channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-# channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
-# if channel_secret is None:
-#     print('Specify LINE_CHANNEL_SECRET as environment variable.')
-#     sys.exit(1)
-# if channel_access_token is None:
-#     print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-#     sys.exit(1)
-channel_secret = 'ff575ec3465dd7962f5e29ba830dec47'
-channel_access_token = 'SzkNL+H1Q1+eAV5iy3rUyueP8IgMA1cxjBe3NlDtlJCY0HJXNKYoZPZ3idB3JGIYbYmv7pQiNTNtfbKxshhUMWy5/w8a9kDWpmI6Z+3wm8IZ9Vcb/xoelnCCvSB8thfp8YhlotZOyFDhmfgWiGBN0gdB04t89/1O/w1cDnyilFU='
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+# channel_secret = 'ff575ec3465dd7962f5e29ba830dec47'
+# channel_access_token = 'SzkNL+H1Q1+eAV5iy3rUyueP8IgMA1cxjBe3NlDtlJCY0HJXNKYoZPZ3idB3JGIYbYmv7pQiNTNtfbKxshhUMWy5/w8a9kDWpmI6Z+3wm8IZ9Vcb/xoelnCCvSB8thfp8YhlotZOyFDhmfgWiGBN0gdB04t89/1O/w1cDnyilFU='
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
 
 def application(environ, start_response):
     # check request path
+    print('application: ', environ['PATH_INFO'])
     if environ['PATH_INFO'] != '/callback':
         start_response('404 Not Found', [])
         return create_body('Not Found')
@@ -72,16 +74,37 @@ def application(environ, start_response):
 
     # if event is MessageEvent and message is TextMessage, then echo text
     for event in events:
+        print('event: ', event)
+        print('event.type: ', event.type)
+        print('event.message: ', event.message)
+
         if not isinstance(event, MessageEvent):
             continue
-        if not isinstance(event.message, TextMessage):
+        if isinstance(event.message, TextMessage):
+            m_text = event.message.text.lower()
+            if m_text == 'hi':
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=event.message.text)
+                )
+            elif m_text == '好看的':
+                image_url = 'https://i.imgur.com/OqESt0b.jpeg'
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    ImageSendMessage(original_content_url=image_url, preview_image_url=image_url)
+                )
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text='I dont know')
+                )
+        if isinstance(event.message, StickerMessage):
+            line_bot_api.reply_message(
+                event.reply_token,
+                StickerSendMessage(package_id=3, sticker_id=random.randint(180, 220))
+            )
+        else:
             continue
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
-
     start_response('200 OK', [])
     return create_body('OK')
 
@@ -97,8 +120,9 @@ if __name__ == '__main__':
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    options = arg_parser.parse_args()
-
-    httpd = wsgiref.simple_server.make_server('', options.port, application)
+    # arg_parser.add_argument('-p', '--port', type=int, default=5000, help='port')
+    # options = arg_parser.parse_args()
+    port = int(os.getenv('PORT', default=8000))
+    httpd = make_server('0.0.0.0', port, application)
+    print('port ', port)
     httpd.serve_forever()
